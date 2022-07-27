ca8b08e0bdb6a88a711f4c4b89d699c988d47f49
Ray Wang <raywang777@foxmail.com>
2022-07-27 13:14:00 +0800

    update source files

---
+++ homework3/homework3/homework3/Passenger.h
@@ -0,0 +1,28 @@
+//
+//  Passenger.h
+//  College Demo
+//
+//  Created by mashujun on 2022/7/18.
+//
+
+#import "Person.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Orders : NSObject
+
+@end
+
+@interface Passenger : Person
+// @property 属性
+// 是否年满 18 岁
+// 历史订单 （数组）
+// 未出行订单 （数组）
+
+// Function 方法
+// 去订票
+
+// 去检票
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ homework3/homework3/homework3/Passenger.m
@@ -0,0 +1,17 @@
+//
+//  Passenger.m
+//  College Demo
+//
+//  Created by mashujun on 2022/7/18.
+//
+
+#import "Passenger.h"
+
+
+@implementation Orders
+
+@end
+
+@implementation Passenger
+
+@end
